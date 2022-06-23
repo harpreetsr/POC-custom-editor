d523b688d7c366fc36bd800abaa3acd7ebfbc175
Harpreet S. Randhawa <harpreetwp@gmail.com>
2022-06-23 13:24:11 +0530

    feat: editor with formik

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,60 @@
-import { Row, Button } from 'antd'
-import { PlusCircleOutlined } from '@ant-design/icons'
+import { Row, Button, Form } from 'antd';
+import { PlusCircleOutlined } from '@ant-design/icons';
 import './App.css';
 import CustomEditor from './components/editor.component';
+import { Field, FieldArray, Formik } from 'formik';
 
 function App() {
   const addMore = () => {
     console.log('Add more...');
-  }
+  };
 
   return (
     <div className="App">
-      <header>
-        POC - Custom Editor
-      </header>
-      <div>
-        <CustomEditor />
-      </div>
-      <Row>
+      <header className="App-header">POC - Custom Editor</header>
+      <Formik
+        initialValues={{ contents: [''] }}
+        onSubmit={(values) => console.log(values)}
+        render={({ values }) => (
+          <Form>
+            <FieldArray
+              name="contents"
+              render={(arrayHelpers) => (
+                <div>
+                  {values.contents && values.contents.length > 0 ? (
+                    values.contents.map((friend, index) => (
+                      <div key={index}>
+                        <Field name={`contents.${index}`} as={CustomEditor} />
+                        <button type="button" onClick={() => arrayHelpers.remove(index)}>
+                          -
+                        </button>
+                        <button type="button" onClick={() => arrayHelpers.insert(index, '')}>
+                          +
+                        </button>
+                      </div>
+                    ))
+                  ) : (
+                    <button type="button" onClick={() => arrayHelpers.push('')}>
+                      {/* show this when user has removed all friends from the list */}
+                      Add a content
+                    </button>
+                  )}
+                  <div>
+                    <Button className="btn-primary" htmlType="submit">
+                      Submit
+                    </Button>
+                  </div>
+                </div>
+              )}
+            />
+          </Form>
+        )}
+      />
+      {/* <Row>
         <Button onClick={addMore}>
           <PlusCircleOutlined />
         </Button>
-      </Row>
-
-
+      </Row> */}
     </div>
   );
 }
--- src/components/editor.component.tsx
+++ src/components/editor.component.tsx
@@ -1,13 +1,23 @@
-import {Row} from 'antd';
-import { Field, Form, Formik, FormikProps } from 'formik';
-import React from 'react'
+import { Radio, Row } from 'antd';
 
 const CustomEditor = () => {
   return (
-    <Row>
-      <textarea className="my-custom-input"></textarea>
-    </Row>
-  )
-}
+    <>
+      <Row>
+        <Radio.Group defaultValue="a" buttonStyle="solid">
+          <Radio.Button value="h">Highlight</Radio.Button>
+          <Radio.Button value="b">Body</Radio.Button>
+          <Radio.Button value="n">Note</Radio.Button>
+          <Radio.Button value="q">Quote</Radio.Button>
+          {/* <Radio.Button value="ma">Merge Above</Radio.Button>
+          <Radio.Button value="mb">Merge Below</Radio.Button> */}
+        </Radio.Group>
+      </Row>
+      <Row>
+        <textarea className="my-custom-input"></textarea>
+      </Row>
+    </>
+  );
+};
 
-export default CustomEditor
+export default CustomEditor;
